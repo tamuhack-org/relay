c4cfa0ae511719b82dfe45c5bceaca2ff91a5228
NitroGuy10 <naviyer10@gmail.com>
2024-05-15 21:55:27 -0500

    Start mailing list page

--- pages/mailing-list/[mailingListId].tsx
+++ pages/mailing-list/[mailingListId].tsx
@@ -8,7 +8,8 @@ export default function Home() {
   
   const [mailingList, setMailingList] = useState<MailingList | null>(null);
   const [listMembers, setListMembers] = useState<ListMember[]>([]);
-  const [failedToFetchTemplate, setFailedToFetchTemplate] = useState(false);
+  const [failedToFetchMailingList, setFailedToFetchMailingList] = useState(false);
+  const [failedToFetchListMembers, setFailedToFetchListMembers] = useState(false);
 
   useEffect(() => {
     if (!mailingListId) {
@@ -17,14 +18,47 @@ export default function Home() {
     async function fetchMailingList() {
       const response = await fetch(`/api/mailing-list/${mailingListId}`);
       if (!response.ok) {
-        setFailedToFetchTemplate(true);
+        setFailedToFetchMailingList(true);
         return;
       }
       const responseData = await response.json();
       setMailingList(responseData.mailingList);
+    }
+    async function fetchListMembers() {
+      const response = await fetch(`/api/mailing-list/${mailingListId}/members`);
+      if (!response.ok) {
+        setFailedToFetchListMembers(true);
+        return;
+      }
+      const responseData = await response.json();
       setListMembers(responseData.listMembers);
     }
     fetchMailingList();
+    fetchListMembers();
   }, [mailingListId]);
 
+  return (
+    <>
+      <h1 className="text-4xl">Mailing List</h1>
+      {failedToFetchMailingList && (
+        <div>Failed to fetch mailing list</div>
+      )}
+      {failedToFetchListMembers && (
+        <div>Failed to fetch list members</div>
+      )}
+      {mailingList ? (
+        <div>
+          <h2>{mailingList.name}</h2>
+          <p>{mailingList.description}</p>
+          <ul>
+            {listMembers.map((listMember) => (
+              <li key={listMember.id}>{listMember.email}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
 }
